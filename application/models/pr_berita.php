edcd43c5937402266e73687f97305055b7983a13
sitorus <r.sitorusdori@gmail.com>
2014-10-12 23:13:18 +0700

    Commit Renhard
    Test Commit

---
+++ application/models/pr_berita.php
@@ -0,0 +1,15 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+	
+	class Pr_berita extends CI_Models {
+		
+		 function __construct()
+		{
+			// Call the Model constructor
+			parent::__construct();
+		}	
+		
+		
+			
+	}
+
+?>
