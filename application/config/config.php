58d923d5ce71548c78dd69eff41872571e42853e
Selamet Subu <selametsubu@yahoo.com>
2014-10-20 11:25:39 +0700

    Config base_url
    Pake kayak gini aja gan biar ga usah di tulis lagi localhost nya tinggal
    nama foldernya aja, nanti kalau sudah masuk server tinggal hapus nama
    folder nya aja kalu ti taruh di root :dancers:

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost:8080/tourism';
+$config['base_url']	= "http://".$_SERVER['HTTP_HOST']."/tourism/";
 
 /*
 |--------------------------------------------------------------------------
